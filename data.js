11bfa4f3bfa5317ae30a3b8be8bf24e19cfcdb70
M4RT1N <mjlynch35@yahoo.com>
2023-03-07 23:37:12 -0600

    Added style for search bar and box shadow, added async function for API

--- data.js
+++ data.js
@@ -12,130 +12,106 @@ var displayData1 = [];
 
 var eventsList = [];
 
-function getCity(city) {
+async function getCity(city) {
     console.clear(); // Clears the console everytime the function is called
     displayData1 = []; // Clearing data in the displayData1 array
-    barData = []; // Clear the barData array
     displayData = []; // Clears the displayData array
 
     // Fetching the openBreweryDB API
     var brewAPI = `https://api.openbrewerydb.org/breweries?by_city=${city}`;
 
     // Calling the openBreweryAPI
-    fetch(brewAPI)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
+    var barResponse = await fetch(brewAPI);
+    var barData = await barResponse.json();
 
-            for (var i = 0; i < data.length; i++) {
-                barData.push(data);
-            }
+    // Gets 5 random bars
+    console.log("Bar picks");
+    var bar = barData;
+    for (var j = 0; j < barData.length; j++) {
+        if (barData.length !== 0) {
+            console.log(bar[j].name);
 
-            // Gets 5 random bars
-            console.log("Bar picks");
-            for (var j = 0; j < barData.length; j++) {
-                if (barData.length !== 0) {
-                    var randomIndex = Math.floor(Math.random() * barData.length);
-                    var indexNum = barData[0][j];
-                    var bar = indexNum;
-                    displayData.push(bar);
-                    console.log(bar.name);
-
-                    //Creates the variables from the API data
-                    var barName = bar.name;
-                    var barAddress = bar.street;
-                    var barCity = bar.city;
-                    var barState = bar.state;
-                    var barZip = bar.postal_code;
-                    var barPhone = bar.phone;
-                    var barWebsite = bar.website_url;
+            //Creates the variables from the API data
+            var barName = bar[j].name;
+            var barAddress = bar[j].street;
+            var barCity = bar[j].city;
+            var barState = bar[j].state;
+            var barZip = bar[j].postal_code;
+            var barPhone = bar[j].phone;
+            var barWebsite = bar[j].website_url;
                    
-                    //Creates the elements that will hold the data
-                    var barDiv = document.createElement("div");
-                    var barNameText = document.createElement("p");
-                    var barAddressText = document.createElement("p");
-                    var barCityStateZipText = document.createElement("p");
-                    var barPhoneText = document.createElement("p");
-                    var barWebsiteText = document.createElement("a");
+            //Creates the elements that will hold the data
+            var barDiv = document.createElement("div");
+            var barNameText = document.createElement("p");
+            var barAddressText = document.createElement("p");
+            var barCityStateZipText = document.createElement("p");
+            var barPhoneText = document.createElement("p");
+            var barWebsiteText = document.createElement("a");
 
-                    //Adds text values to the data elements
-                    barNameText.textContent = barName;
-                    barAddressText.textContent = barAddress;
-                    barCityStateZipText.textContent = barCity + ", " + barState + " " + barZip;
-                    barPhoneText.textContent = barPhone;
-                    barWebsiteText.textContent = barWebsite;
+            //Adds text values to the data elements
+            barNameText.textContent = barName;
+            barAddressText.textContent = barAddress;
+            barCityStateZipText.textContent = barCity + ", " + barState + " " + barZip;
+            barPhoneText.textContent = barPhone;
+            barWebsiteText.textContent = barWebsite;
                     
-                    //Appends the brewery data to the bar div elements, and then the main container 
-                    barDiv.setAttribute("class", "bar");
-                    document.getElementById("breweries").appendChild(barDiv);
-                    barDiv.appendChild(barNameText);
-                    barDiv.appendChild(barAddressText);
-                    barDiv.appendChild(barCityStateZipText);
-                    barDiv.appendChild(barPhoneText);
-                    barDiv.appendChild(barWebsiteText);
-                    barWebsiteText.setAttribute("href", barWebsite);
-
-
-                } else {
-                    sorryNoBars = document.createElement("p");
-                    sorryNoBars.textContent = "Sorry there are no bars in your area.";
-                    document.getElementById("breweries").appendChild(sorryNoBars);
-                    console.log("Sorry there are no bars in your area");
-                }
-            }
-        })
+            //Appends the brewery data to the bar div elements, and then the main container 
+            barDiv.setAttribute("class", "bar");
+            document.getElementById("breweries").appendChild(barDiv);
+            barDiv.appendChild(barNameText);
+            barDiv.appendChild(barAddressText);
+            barDiv.appendChild(barCityStateZipText);
+            barDiv.appendChild(barPhoneText);
+            barDiv.appendChild(barWebsiteText);
+            barWebsiteText.setAttribute("href", barWebsite);
+        } else {
+            sorryNoBars = document.createElement("p");
+            sorryNoBars.textContent = "Sorry there are no bars in your area.";
+            document.getElementById("breweries").appendChild(sorryNoBars);
+            console.log("Sorry there are no bars in your area");
+        }
+    }
 
     // Fetching the seatgeekAPI
-    fetch(`https://api.seatgeek.com/2/events?venue.city=${input.value}&client_id=${sgAPIKEY}`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data.events.length);
-            for (var i = 0; i < data.events.length; i++) {
-                // Appending the data event elements to the eventsList array
-                eventsList.push(data.events[i]);
-                console.log(eventsList);
+    var eventResponse = await fetch(`https://api.seatgeek.com/2/events?venue.city=${input.value}&client_id=${sgAPIKEY}`);
+    var eventData = await eventResponse.json();
 
-                //Creates the variables from the API Data
-                var eventImageUrl = data.events[i].performers[0].image;
-                var eventTitle = data.events[i].title;
-                var eventLocation = data.events[i].venue.name;
-                var eventURL = data.events[i].url;
+    //console.log(barData.events.length);
+    for (var i = 0; i < eventData.events.length; i++) {
+        //Creates the variables from the API Data
+        var eventImageUrl = eventData.events[i].performers[0].image;
+        var eventTitle = eventData.events[i].title;
+        var eventLocation = eventData.events[i].venue.name;
+        var eventURL = eventData.events[i].url;
 
-                //Creares the elements that will hold the data
-                var eventDiv = document.createElement("div");
-                var eventImage = document.createElement("img");
-                var eventTitleText = document.createElement("p");
-                var eventLocationText = document.createElement("p");
-                var eventUrlText = document.createElement("a");
+        //Creares the elements that will hold the data
+        var eventDiv = document.createElement("div");
+        var eventImage = document.createElement("img");
+        var eventTitleText = document.createElement("p");
+        var eventLocationText = document.createElement("p");
+        var eventUrlText = document.createElement("a");
                 
-                //Add the values to the event data elemnts
-                eventImage.setAttribute("src", eventImageUrl);
-                eventUrlText.setAttribute("href", eventURL);
-                eventTitleText.textContent = eventTitle;
-                eventLocationText.textContent = eventLocation;
-                eventUrlText.textContent = "Click for Ticket Info";
+        //Add the values to the event data elemnts
+        eventImage.setAttribute("src", eventImageUrl);
+        eventUrlText.setAttribute("href", eventURL);
+        eventTitleText.textContent = eventTitle;
+        eventLocationText.textContent = eventLocation;
+        eventUrlText.textContent = "Click for Ticket Info";
     
-                //Appends the events data to the events div elements, and then the main container
-                eventDiv.setAttribute("class", "event");
-                document.getElementById("events").appendChild(eventDiv);
-                eventDiv.appendChild(eventImage);
-                eventDiv.appendChild(eventTitleText);
-                eventDiv.appendChild(eventLocationText);
-                eventDiv.appendChild(eventUrlText);
-
-            }
+        //Appends the events data to the events div elements, and then the main container
+        eventDiv.setAttribute("class", "event");
+        document.getElementById("events").appendChild(eventDiv);
+        eventDiv.appendChild(eventImage);
+        eventDiv.appendChild(eventTitleText);
+        eventDiv.appendChild(eventLocationText);
+        eventDiv.appendChild(eventUrlText);
+    }
 
             // var filteredEvents = eventsList.filter((value, index, self) => 
             //     index === self.findIndex((v) => v.title === value.title)
             // );
 
             // console.log("data",filteredEvents);
-
-        })
 
     // Calling the displayDataOnPage function so that all results are filterd and displayed
     displayDataOnPage();
@@ -145,8 +121,6 @@ function getCity(city) {
 
 // This will filter the elements in the displayData array so that there are no duplicates
 function displayDataOnPage() {
-    document.querySelectorAll(".bar").forEach(barBox => barBox.remove());
-    document.querySelectorAll(".event").forEach(eventBox => eventBox.remove());
     var displayOnPage = displayData.filter((value, index) => {
         return displayData.indexOf(value) === index;
     });
@@ -164,9 +138,10 @@ function displayDataOnPage() {
     //console.log("display",displayOnPage);
 }
 
-console.log("Bar Data: ", barData);
 
 btn.addEventListener("click", function () {
+    document.querySelectorAll(".bar").forEach(barBox => barBox.remove());
+    document.querySelectorAll(".event").forEach(eventBox => eventBox.remove());
     // function will only be called if the user has entered a city
     if (input.value !== "") {
         // Calls the function with the the input given when user clicks submit
